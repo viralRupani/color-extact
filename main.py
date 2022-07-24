083d33238a2e53a00e62bf0729d85836a4a868ac
viral rupani <viralrupani12017@gmail.com>
2022-07-24 14:59:57 +0530

    restricting for jpg/png

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import numpy as np
 import os
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SECRET_KEY'] = 'poiufpe;oiuf'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['UPLOAD_FOLDER'] = 'static/image/'
 
 
